15f10e09df69c42649cf51ee57ac39a04d1672f9
mongesan <syun1.mongesan@gmail.com>
2024-05-01 20:14:27 +0900

    Classification of timer method

--- Beginner.dart
+++ Beginner.dart
@@ -2,56 +2,43 @@ import "dart:convert";
 import "dart:io";
 import 'dart:async';
 
-bool timerMoving = true;
-int timerSaved = 0;
-Future<String?> getUserInput(String message) async {
-  print(message);
-  return stdin.readLineSync();
-}
-
-Future<void> count(int timerInitial) async {
-  int time = timerInitial;
-  timerMoving = true;
-  while (timerMoving) {
-    await Future.delayed(Duration(seconds: 1));
-    time += 1;
-    // time in second
-    String sec = (time % 60).toString();
-    String min = (time ~/ 60).toString();
-    if (sec.length == 1) {
-      sec = '0' + sec;
-    }
-    if (min.length == 1) {
-      min = '0' + min;
-    }
-    print("$min:$sec");
+class Timer {
+  bool ifcounting = false;
+  int time = 0;
+  String Message =
+      "To start/restart stopwatch, type 'start' or 's'\nTo stop, type 'stop' or 'break'\nTo reset, type 'reset' or 'r'";
+  void initialize() {
+    print('----------\nSTOP WATCH\n----------');
+    print(Message);
+    print('----------');
   }
-  timerSaved = time;
+
+  Future<void> count() async {
+    while (ifcounting) {
+      await Future.delayed(Duration(seconds: 1));
+      time += 1;
+    }
+  }
 }
 
-Stream<String> readline() =>
-    stdin.transform(utf8.decoder).transform(const LineSplitter());
+// String sec = (time % 60).toString();
+//       String min = (time ~/ 60).toString();
+//       if (sec.length == 1) {
+//         sec = '0' + sec;
+//       }
+//       if (min.length == 1) {
+//         min = '0' + min;
+//       }
+//       print("$min:$sec");
+// Future<String?> getUserInput(String message) async {
+//   print(message);
+//   return stdin.readLineSync();
+// }
 
-String Message =
-    "To start/restart stopwatch, type 'start' or 's'\nTo stop, type 'stop' or 'break'\nTo reset, type 'reset' or 'r'";
 void main() async {
-  print('----------\nSTOP WATCH\n----------');
-  print(Message);
+  Stream<String> readline() =>
+      stdin.transform(utf8.decoder).transform(const LineSplitter());
   readline().listen(getstdin);
-}
-
-void getstdin(String line) {
-  if (line == 'r' || line == 'reset' || line == "RESET") {
-    print("TIMER RESET!\n---------");
-    timerSaved = 0;
-    timerMoving = false;
-  } else if (line == 's' || line == 'start' || line == "START") {
-    count(timerSaved);
-  } else if (line == 'stop' || line == "break") {
-    print('TIMER STOPPED!\n----------');
-    timerMoving = false;
-    print(Message);
-  } else {
-    print(Message);
-  }
+  var timer = Timer();
+  timer.initialize();
 }
