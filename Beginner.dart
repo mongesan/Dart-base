9ee595753942f0a4cc24456c983f3887d170a12d
mongesan <syun1.mongesan@gmail.com>
2024-07-24 11:33:18 +0900

    Reconfiguration based on refactoring

--- Beginner.dart
+++ Beginner.dart
@@ -6,7 +6,8 @@ class Timer {
   bool ifCounting = false;
   int elapsedTime = 0;
   String Message =
-      "To start/restart stopwatch, type 'start' or 's'\nTo stop, type 'stop' or 'break'\nTo reset, type 'reset' or 'r'";
+      "To start stopwatch, type 'start' or 's'\nTo stop, type 'stop' or 'break'\nTo reset, type 'reset' or 'r'";
+  
   void initialize() {
     print('----------\nSTOP WATCH\n----------');
     print(Message);
@@ -26,25 +27,55 @@ class Timer {
       elapsedTime += 1;
     }
   }
-}
 
-// Future<String?> getUserInput(String message) async {
-//   print(message);
-//   return stdin.readLineSync();
-// }
-
-void main() async {
-  Stream<String> readline() =>
-      stdin.transform(utf8.decoder).transform(const LineSplitter());
-
-  Future<String?> getUserInput(String message) async {
-    print(message);
-    return stdin.readLineSync();
+  Future<void> getStdin() async {
+    await stdin
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .forEach((line) {
+      lineOperation(line);
+    });
   }
 
-  readline().listen(getstdin);
+  lineOperation(line) {
+    if (line == 'r' || line == 'reset' || line == "RESET") {
+      reset();
+    } else if (line == 's' || line == 'start' || line == "START") {
+      start();
+    } else if (line == 'x' || line == 'stop' || line == "break") {
+      stop();
+    }
+  }
+
+  reset() {
+    ifCounting = false;
+    elapsedTime = 0;
+    print("TIMER RESET!\n---------");
+  }
+
+  start() {
+    if (ifCounting) {
+      print('Timer already started');
+    } else {
+      ifCounting = true;
+      count();
+      print('TIMER START!\n----------');
+    }
+  }
+
+  stop() {
+    if (ifCounting) {
+      ifCounting = false;
+      print('TIMER STOP!\n----------');
+    } else {
+      print('Timer already stopped');
+    }
+  }
+}
+
+void main() async {
   var timer = Timer();
   timer.initialize();
-  timer.ifCounting = true;
   timer.count();
+  timer.getStdin();
 }
